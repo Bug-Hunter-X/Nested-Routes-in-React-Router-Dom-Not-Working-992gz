cc048ee17adf6942e7d34ee5c252e5baf5995d2f
BugHunter-X <askzenai@gmail.com>
2025-01-06 20:02:07 +0100

    Add AppSolution.js file

---
+++ AppSolution.js
@@ -0,0 +1,27 @@
+```javascript
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />}>
+          <Route path="details" element={<ContactDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() { return <h1>Home</h1>; }
+function About() { return <h1>About</h1>; }
+function Contact() { return <h1>Contact</h1>; }
+
+function ContactDetails() {
+  const {param} = useParams();
+  return <h1>Contact Details {param}</h1>;
+}
+export default App;
+```
